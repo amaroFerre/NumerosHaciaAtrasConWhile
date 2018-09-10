654ced94eb33ab4ee56582428cdaca0704f333c3
Amarico <Amarico@equipo-zalman>
2018-09-10 19:53:04 +0200

    Cambio el valor de la variable de 320 a 340 para que me muestre el número 320

--- src/numeroshaciaatrasconwhile/NumerosHaciaAtrasConWhile.java
+++ src/numeroshaciaatrasconwhile/NumerosHaciaAtrasConWhile.java
@@ -20,9 +20,20 @@ public class NumerosHaciaAtrasConWhile {
     //Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
     //utilizando un bucle while.
     
-    //Creo una variable de tipo entero con el valor 320
+    //Creo una variable de tipo entero con el valor 340
     
     int numero = 320;
+    
+    // Inicio bucle While
+    
+    while (numero > 160) {
+
+    numero = numero - 20;
+      
+    
+      System.out.println(numero);
+    }
+    
   }
 
 }
